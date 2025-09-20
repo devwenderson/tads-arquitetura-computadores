f957d106c80d97fba71a951db6e7f74ed3a97ad2
devwenderson <wend.devdesigner@gmail.com>
2025-09-20 09:43:58 -0300

    update q6.asm

--- lista-01/q6.asm
+++ lista-01/q6.asm
@@ -1,9 +1,37 @@
 .text
-main: 	addi $2, $0, 5
+main: 	addi $2, $0, 5			# n
+	syscall
+			
+	add $8, $0, $2			# $8 = n
+	
+	addi $10, $0, 10		# $10 = 10
+	
+	div $8, $10			# n / 10
+	mfhi $11			# $11 = resto
+	mflo $8				# $8 = quociente
+	
+	add $12, $0, $11		# armazena resto em $12
+	
+	div $8, $10			# quociente / 10
+	mfhi $11
+	mflo $8
+	
+	add $13, $0, $11		# armazena resto em $13
+	
+	div $8, $10
+	mfhi $11
+	
+	add $14, $0, $11		# armazena resto em $14
+	
+	add $4, $0, $14			# Início da exibição dos restos
+	addi $2, $0, 1
 	syscall
 	
-	srl $4, $2, 3
+	add $4, $0, $13
+	addi $2, $0, 1
+	syscall
 	
+	add $4, $0, $12			# Fim da exibição dos restos
 	addi $2, $0, 1
 	syscall
 	
