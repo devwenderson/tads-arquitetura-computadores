e232a9e77f2f884af2ac6feaca67bea7ef41d1af
devwenderson <wend.devdesigner@gmail.com>
2025-11-14 12:52:06 -0300

    add: código da prova 4

--- provas/codigo-prova-4.asm
+++ provas/codigo-prova-4.asm
@@ -1,26 +1,25 @@
 .text
 main:
-    # int n = 8;
-    # int a[n] = {3, 1, -2, 5, 7, 10, 0, 2};
-    # int maior_soma = a[0] + a[1];
-    # int pos = 0;
-    # int x, y;
-    
-    # for (int i=0; i<n-1; i++) {
-    #    for (int j=i+1; j<i+2; j++) {
-    #         if (a[i] + a[j] > maior_soma) {
-    #             maior_soma = a[i] + a[j];
-    #             pos = i;
-    #             x = a[i];
-    #             y = a[j];
-    #        }
-    #     }
-    # }
-
-    # cout << maior_soma << "\n";
-    # cout << pos << "\n";
-    # cout << "X: " << x << " " << "Y: " << y << "\n";
-    	addi $8, $0, 10
+	# $8: n
+	# $25: n
+	# $9: endereço
+	# $10: i
+	# $15: j
+	# $11: maior_soma
+	# $12: soma_atual
+	# $13: qtd
+	# $14: pos
+	# $16: a_qtd
+	# $19: inidice i
+	# $22: vet[i]
+	# $21: vet[j]
+	
+    	addi $2, $0, 5 # n
+    	syscall
+    	
+    	add $8, $0, $2 # n
+    	add $25, $0, $2 # copia de n
+    	
     	lui $9, 0x1001
 for1:
 	beq $8, $0, endFor1
@@ -35,9 +34,8 @@ for1:
 	j for1
 
 endFor1:    
-
 	lui $9, 0x1001
-	addi $8, $0, 10	
+	add $8, $0, $25	
 	
 forImprimeSeq:
 	beq $8, $0, endForImprimeSeq
@@ -56,39 +54,36 @@ forImprimeSeq:
 	
 endForImprimeSeq:	
 	lui $9, 0x1001
-	addi $8, $0, 9 # n - 1
-	addi $10, $0, 0 # i
+	add $8, $0, $25 # n
+	addi $10, $0, 0 # i = 0
+	addi $13, $0, 1 # qtd
+	addi $14, $0, 0 # pos	
 	
-	lw $11, 0($9)
-	lw $12, 4($9)
-	add $11, $11, $12 # maior_soma
-	
-	lui $9, 0x1001
+	lw $11, 0($9) # maior_soma = vet[0]
 forI:
-	beq $10, $8, endForI
-	
-	add $15, $10, 1 # j=i+1
-	add $16, $10, 2 # i+2
-	
+	beq $10, $8, endForI # for (i=0; i<n; i++)	
+	addi $15, $10, 1 # j=i+1
+	sll $19, $10, 2 # indice i
+	add $9, $9, $19
+	lw $22, 0($9) # $22 = vet[i]
+	lui $9, 0x1001
+	add $12, $0, $22 # $12 = soma_atual
+	addi $16, $0, 1 # a_qtd
+	add $14, $0, $10 # pos = i
 forJ:	
-	beq $15, $16, proximoPassoForI # j < i+2
+	beq $15, $25, proximoPassoForI # for (j=i+1; j<n; j++)
 	
-	sll $14, $10, 2 # indice i
-	sll $13, $15, 2 # indice j
-	
-	add $9, $9, $14 
-	lw $24, 0($9) # a[i]
+	sll $18, $15, 2 # indice j
+	add $9, $9, $18
+	lw $21, 0($9) # vet[j]
 	lui $9, 0x1001
+	add $12, $12, $21 # soma_atual = soma_atual + vet[j]
+	addi $16, $16, 1 # a_qtd++
 	
-	add $9, $9, $13
-	lw $25, 0($9) # a[j]
-	lui $9, 0x1001
-	
-	add $24, $24, $25
-	slt $25, $11, $24
-	bne $25, $0, trocaMaior
+	slt $20, $11, $12 # if (maior_soma < soma_atual)
+	bne $20, $0, trocaMaior 
 
-proximoPassoForJ:	
+proximoPassoForJ:
 	addi $15, $15, 1
 	j forJ
 	
@@ -97,8 +92,8 @@ proximoPassoForI:
 	j forI
 	
 trocaMaior:
-	add $11, $0, $24 # Maior soma
-	add $17, $0, $8 # posicao do primeiro numero do par
+	add $11, $0, $12 # Maior soma
+	add $13, $0, $16
 	j proximoPassoForJ
 	
 endForI:
@@ -106,11 +101,15 @@ endForI:
 	addi $2, $0, 11
 	syscall
 	
-	addi $4, $0, 'p'
-	addi $2, $0, 11
+	add $4, $0, $11 # cout << maior_soma;
+	addi $2, $0, 1
 	syscall
 	
-	add $4, $0, $17
+	addi $4, $0, '\n'
+	addi $2, $0, 11
+	syscall
+		
+	add $4, $0, $14 # cout << pos;
 	addi $2, $0, 1
 	syscall
 	
@@ -118,8 +117,12 @@ endForI:
 	addi $2, $0, 11
 	syscall
 	
-	add $4, $0, $11
+	add $4, $0, $13 # cout << qtd;
 	addi $2, $0, 1
+	syscall
+	
+	addi $4, $0, '\n'
+	addi $2, $0, 11
 	syscall
 
 	addi $2, $0, 10
