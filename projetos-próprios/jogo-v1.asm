677cd964ba2665f4ad3590ba5f9eb1b92bdf28b2
devwenderson <wend.devdesigner@gmail.com>
2025-11-23 14:50:15 -0300

    add: tentativa de andar personagem

--- projetos-próprios/jogo-v1.asm
+++ projetos-próprios/jogo-v1.asm
@@ -1,8 +1,28 @@
+.data
+.word 0x00000000 # PRETO
+.word 0x00639bff # AZUL CLARO
+.word 0x00ffffff # BRANCO
+.word 0x001d2e97 # AZUL MARINHO
+.word 0x00847e87 # CINZA
+
+
 .text
 main:
 	lui $4, 0x1001
 	jal desenhaFundo
-fim:
+	
+	lui $4, 0x1001
+	addi $8, $0, 16
+# TENTATIVA DE FAZER O OBJETO ANDAR
+forAndar:
+	beq $8, $0, fim
+	jal desenhaMordecai
+	jal desenhaFundo
+	add $4, $4, 4
+	addi $8, $8, -1
+	j forAndar
+	
+fim:	
 	addi $2, $0, 10
 	syscall
 
@@ -28,3 +48,48 @@ for1:
 	j for1
 endFor1:
 	jr $31 # Volta para a instrução de $31
+
+############################################
+# === Rotina para desenhar o Mordecai ===  #
+# Entradas:                                #	 				
+#	$4: endereço de início do desenho  #
+# Saída: 
+#        void                              #
+# Usa (sem preservar):                     #   
+#	$16: alcance do laço               #
+#	$17: endereço local dos pixels     #
+#	$18: cor local                     #
+#
+# De uma linha para outra soma 128
+#
+#
+#
+############################################
+desenhaMordecai:
+	add $17, $0, $4 # endereço local dos pixels
+	ori $18, $0, 0x00000000 # cor local
+
+	# INÍCIO DO TOPO DA CABEÇA
+	sw $18, 16($17)
+	sw $18, 528($17)
+	sw $18, 532($17)
+	sw $18, 1040($17) # Lateral esquerda
+	sw $18, 1044($17)
+	sw $18, 1048($17)
+		
+	# DAQUI PARA BAIXO TEM UMA REPETIÇÃO DE SOMAS
+	sw $18, 1552($17) # Lateral esquerda (+512)
+	sw $18, 1564($17) # (+4)
+	sw $18, 2064($17) # Lateral esquerda (+512)
+	sw $18, 2080($17) # (+4)
+	sw $18, 2576($17) # Lateral esquerda (+512)
+	sw $18, 2596($17) # (+4)
+	sw $18, 3088($17) # Lateral esquerda (+512)
+	sw $18, 3112($17) # (+4)
+	sw $18, 3600($17) # Lateral esquerda (+512)
+	sw $18, 3628($17) # (+4)
+	sw $18, 4112($17) # Lateral esquerda (+512)
+	sw $18, 4144($17) # (+4) 
+	# FIM DO TOPO DA CABEÇA
+	
+	jr $31
