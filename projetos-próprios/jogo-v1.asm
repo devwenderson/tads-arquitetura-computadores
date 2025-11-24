c2ec47383a20fdf4cd903fbd7deef948d1d40c51
devwenderson <wend.devdesigner@gmail.com>
2025-11-24 16:59:33 -0300

    update: desenha mordecai

--- projetos-próprios/jogo-v1.asm
+++ projetos-próprios/jogo-v1.asm
@@ -46,21 +46,18 @@ endFor1:
 # === Rotina para desenhar o Mordecai ===  #
 # Entradas:                                #	 				
 #	$4: endereço de início do desenho  #
-# Saída: 
+# Saída:                                   #
 #        void                              #
 # Usa (sem preservar):                     #   
-#	$16: alcance do laço               #
 #	$17: endereço local dos pixels     #
 #	$18: cor local                     #
-#
-# De uma linha para outra soma 128
-#
-#
-#
+#                                          #
+# De uma linha para outra soma 512         #
+#                                          #
 ############################################
 desenhaMordecai:
 	add $17, $0, $4 # endereço local dos pixels
-	ori $18, $0, 0x00000000 # cor local
+	ori $18, $0, 0x0000 # cor local
 
 	# INÍCIO DO TOPO DA CABEÇA
 	sw $18, 16($17)
@@ -149,7 +146,106 @@ desenhaMordecai:
 	sw $18, 4648($17)
 	# Fim do Olho esquerdo
 	
+	ori $18, $0, 0x639b
+	sll $18, $18, 8
+	ori $18, $18, 0xff 
+	
+	#### A PARTIR DAQUI É A COLORAÇÃO DO MORDECAI ####
+	# INÍCIO DO TOPO DA CABEÇA
+	# DAQUI PARA BAIXO TEM UMA REPETIÇÃO DE SOMAS
+	sw $18, 1556($17) # Lateral interna esquerda
+	sw $18, 1560($17)
+	sw $18, 2068($17) # Lateral interna esquerda
+	sw $18, 2072($17)
+	sw $18, 2076($17)
+	sw $18, 2580($17)
+	sw $18, 2584($17)
+	sw $18, 2588($17)
+	sw $18, 2592($17) 
+	sw $18, 3092($17) # Lateral interna esquerda
+	sw $18, 3096($17)
+	sw $18, 3100($17)
+	sw $18, 3104($17)
+	sw $18, 3108($17)
+	sw $18, 3084($17)
+	sw $18, 3596($17) # Lateral interna esquerda
+	sw $18, 3600($17)
+	sw $18, 3604($17)
+	sw $18, 3608($17)
+	sw $18, 3612($17)
+	sw $18, 3616($17)
+	sw $18, 4108($17) # Lateral interna esquerda
+	sw $18, 4112($17)
+	sw $18, 4116($17)
+	sw $18, 4120($17)
+	sw $18, 4124($17)
+	sw $18, 4616($17) # Lateral interna esquerda
+	sw $18, 4620($17)
+	sw $18, 4624($17)
+	sw $18, 4628($17)
+	sw $18, 4632($17)
+	sw $18, 4636($17)
+	sw $18, 5128($17) # Lateral interna esquerda
+	sw $18, 5132($17)
+	sw $18, 5136($17)
+	sw $18, 5140($17)
+	sw $18, 5144($17)
+	sw $18, 5148($17)
+	sw $18, 5152($17)
+	sw $18, 5640($17) # Lateral interna esquerda
+	sw $18, 5644($17)
+	sw $18, 5648($17)
+	sw $18, 5652($17)
+	sw $18, 5656($17)
+	sw $18, 6156($17) # Lateral interna esquerda
+	sw $18, 6160($17)
+	sw $18, 6164($17)
+	
+	ori $18, $0, 0xffff
+	sll $18, $18, 8
+	ori $18, $18, 0xcc
 	
 	
+	sw $18, 4132($17) # Lateral interna do olho esquerdo
+	sw $18, 4136($17)
+	sw $18, 4644($17) # Lateral interna do olho esquerdo
 	
+	sw $18, 4144($17) # Lateral interna do olho direito
+	sw $18, 4148($17)
+	sw $18, 4656($17) # Lateral interna do olho direito
+	
+	sw $18, 6172($17) # Lateral interna da bochecha
+	sw $18, 6176($17)
+	sw $18, 6180($17)
+	sw $18, 6680($17) # Lateral interna da bochecha
+	sw $18, 6684($17)
+	sw $18, 6688($17)
+	sw $18, 6692($17)
+	sw $18, 6696($17)
+	sw $18, 7188($17) # Lateral interna da bochecha
+	sw $18, 7192($17)
+	sw $18, 7196($17)
+	sw $18, 7200($17)
+	sw $18, 7204($17)
+	sw $18, 7208($17)
+	# FIM DA CABEÇA
+	
+	# PINTURA DA BOCA
+	ori $18, $0, 0x847e
+	sll $18, $18, 8
+	ori $18, $18, 0x87	
+	
+	sw $18, 5676($17) # Lateral interna da boca
+	sw $18, 5680($17)
+	sw $18, 5684($17)
+	sw $18, 5688($17)
+	sw $18, 5692($17)
+	sw $18, 5696($17)
+	sw $18, 5700($17)
+	sw $18, 5704($17)
+	sw $18, 6188($17) # Lateral interna da boca
+	sw $18, 6192($17)
+	sw $18, 6196($17)
+	sw $18, 6200($17)
+	# FIM PINTURA DA BOCA	
 	jr $31
